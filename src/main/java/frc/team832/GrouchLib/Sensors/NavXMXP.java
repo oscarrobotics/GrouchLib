fab5bae9a3831242aefd92273fff771b62f3be14
Banks Troutman <btrout.dhrs@gmail.com>
2019-02-21 13:15:35 -0500

    Refactored NavX construction

---
+++ src/main/java/frc/team832/GrouchLib/Sensors/NavXMXP.java
@@ -0,0 +1,16 @@
+package frc.team832.GrouchLib.Sensors;
+
+public class NavXMXP extends NavXMicro {
+
+    public NavXMXP(NavXPort port) {
+        super(port);
+    }
+
+    public NavXMXP(NavXPort port, byte updateRate) {
+        super(port, updateRate);
+    }
+
+
+
+    // TODO: add MXP IO functions?
+}
