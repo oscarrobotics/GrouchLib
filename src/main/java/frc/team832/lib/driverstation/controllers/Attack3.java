cc0f30ea04ee585007dfd39935521f10c9abaed6
Oscar Laptop 1 <programming@roswellrobotics.com>
2019-11-21 19:11:35 -0500

    Added Joystick wrappers for better tank drive

---
+++ src/main/java/frc/team832/lib/driverstation/controllers/Attack3.java
@@ -0,0 +1,29 @@
+package frc.team832.lib.driverstation.controllers;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class Attack3 extends Joystick {
+	/**
+	 * Construct an instance of a joystick. The joystick index is the USB port on the drivers
+	 * station.
+	 *
+	 * @param port The port on the Driver Station that the joystick is plugged into.
+	 */
+	public Attack3 (int port) {
+		super(port);
+	}
+
+	public final JoystickButton trigger = new JoystickButton(this, 1);
+	public final JoystickButton two = new JoystickButton(this, 2);
+	public final JoystickButton three = new JoystickButton(this, 3);
+	public final JoystickButton four = new JoystickButton(this, 4);
+	public final JoystickButton five = new JoystickButton(this, 5);
+	public final JoystickButton six = new JoystickButton(this, 6);
+	public final JoystickButton seven = new JoystickButton(this, 7);
+	public final JoystickButton eight = new JoystickButton(this, 8);
+	public final JoystickButton nine = new JoystickButton(this, 9);
+	public final JoystickButton ten = new JoystickButton(this, 10);
+	public final JoystickButton eleven = new JoystickButton(this, 11);
+
+}
