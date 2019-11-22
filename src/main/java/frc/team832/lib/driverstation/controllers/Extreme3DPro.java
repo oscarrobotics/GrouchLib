7cfd1bd951dee44416cd328d6d5d808839819df9
Oscar Laptop 1 <programming@roswellrobotics.com>
2019-11-21 19:11:35 -0500

    Added Joystick wrappers for better tank drive

---
+++ src/main/java/frc/team832/lib/driverstation/controllers/Extreme3DPro.java
@@ -0,0 +1,19 @@
+package frc.team832.lib.driverstation.controllers;
+
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class Extreme3DPro extends Attack3 {
+	/**
+	 * Construct an instance of a joystick. The joystick index is the USB port on the drivers
+	 * station.
+	 *
+	 * @param port The port on the Driver Station that the joystick is plugged into.
+	 */
+	public Extreme3DPro (int port) {
+		super(port);
+		setTwistChannel(2);
+		setThrottleChannel(3);
+	}
+
+	public final JoystickButton twelve = new JoystickButton(this, 12);
+}
