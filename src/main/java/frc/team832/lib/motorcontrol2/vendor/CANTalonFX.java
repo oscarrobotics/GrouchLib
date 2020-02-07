05e166c320c112dc5ec11bd07057cdb4c17ae831
Banks T <btrout.dhrs@gmail.com>
2020-02-07 03:22:43 -0500

    Update motorcontrol classes for CAN safety, add MusicBox

--- src/main/java/frc/team832/lib/motorcontrol2/vendor/CANTalonFX.java
+++ src/main/java/frc/team832/lib/motorcontrol2/vendor/CANTalonFX.java
@@ -6,14 +6,17 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import frc.team832.lib.CANDevice;
 import frc.team832.lib.motorcontrol.NeutralMode;
-import frc.team832.lib.motorcontrol2.PowerManagedMC;
 import frc.team832.lib.motorcontrol2.SmartMC;
 import frc.team832.lib.motors.Motor;
 import frc.team832.lib.util.ClosedLoopConfig;
 
-public class CANTalonFX extends PowerManagedMC<TalonFX> {
+@SuppressWarnings("unused")
+public class CANTalonFX implements SmartMC<TalonFX> {
 
     private final TalonFX _talon;
+    private final int _canID;
+
+    private boolean _onBus;
 
     private ControlMode _ctrlMode;
     private SupplyCurrentLimitConfiguration inputCurrentConfig = new SupplyCurrentLimitConfiguration(true, 40, 0, 0);
@@ -22,10 +25,11 @@ public class CANTalonFX extends PowerManagedMC<TalonFX> {
 
     public CANTalonFX(int canId) {
         _talon = new TalonFX(canId);
+        _canID = canId;
         _ctrlMode = ControlMode.Disabled;
 
-        boolean onBus = _talon.getBusVoltage() > 0.0; // TODO: better way to do this?
-        CANDevice.addDevice(new CANDevice(canId, onBus, "Talon FX"));
+        _onBus = _talon.getBusVoltage() > 0.0; // TODO: better way to do this?
+        CANDevice.addDevice(new CANDevice(_canID, _onBus, "Talon FX"));
     }
 
     @Override
@@ -34,127 +38,153 @@ public class CANTalonFX extends PowerManagedMC<TalonFX> {
     }
 
     @Override
-    public void follow(SmartMC masterMC) {
-        _ctrlMode = ControlMode.Follower;
-        if (masterMC instanceof  CANTalonFX) {
-            _talon.follow(((CANTalonFX) masterMC).getBaseController());
-        } else if (masterMC instanceof CANTalonSRX) {
-            _talon.follow(((CANTalonSRX) masterMC).getBaseController());
-        } else {
-            _talon.set(_ctrlMode, masterMC.getCANID());
-        }
-    }
-
-    @Override
-    public double getInputVoltage() {
-        return _talon.getBusVoltage();
-    }
-
-    @Override
-    public double getOutputVoltage() {
-        return _talon.getMotorOutputVoltage();
-    }
-
-    @Override
-    public double getInputCurrent() {
-        return _talon.getSupplyCurrent();
-    }
-
-    @Override
-    public double getOutputCurrent() {
-        return _talon.getStatorCurrent();
-    }
-
-    @Override
-    public void setNeutralMode(NeutralMode mode) {
-        _talon.setNeutralMode(mode == NeutralMode.kBrake ?
-         com.ctre.phoenix.motorcontrol.NeutralMode.Brake :
-          com.ctre.phoenix.motorcontrol.NeutralMode.Coast);
-    }
-
-    @Override
-    public int getCANID() {
-        return 0;
-    }
-
-    @Override
-    public void wipeSettings () {
-        _talon.configFactoryDefault();
-    }
-
-    @Override
-    public void limitInputCurrent(int currentLimit) {
-        inputCurrentConfig.currentLimit = currentLimit;
-        _talon.configSupplyCurrentLimit(inputCurrentConfig);
-    }
-
-    public void limitOutputCurrent(int currentLimit) {
-        outputCurrentConfig.currentLimit = currentLimit;
-        _talon.configStatorCurrentLimit(outputCurrentConfig);
-    }
-
-    @Override
-    public double getSensorPosition () {
-        return _talon.getSelectedSensorPosition() / 2048.0;
-    }
-
-    @Override
-    public double getSensorVelocity () {
-        return (_talon.getSelectedSensorVelocity() / 2048.0) * 600;
-    }
-
-    @Override
-    public void setVelocity (double v) {
-        _talon.configMotionCruiseVelocity((int) v);
-    }
-
-    @Override
-    public void rezeroSensor () {
-        _talon.setSelectedSensorPosition(0);
-    }
-
-    @Override
-    public void setEncoderPosition (double position) { _talon.setSelectedSensorPosition((int)position); }
-
-    @Override
-    public void setSensorPhase(boolean phase) { _talon.setSensorPhase(phase); }
-
-    @Override
-    public void set(double power) {
-        _ctrlMode = ControlMode.PercentOutput;
-        _talon.set(_ctrlMode, power);
-    }
-
-    @Override
-    public double get() {
-        return _talon.getMotorOutputPercent();
-    }
-
-    @Override
-    public void stop() {
-        _talon.set(ControlMode.PercentOutput, 0);
-    }
-
-    @Override
-    public void setInverted(boolean inverted) {
-        _talon.setInverted(inverted);
-    }
-
-    @Override
-    public boolean getInverted() {
-        return _talon.getInverted();
-    }
-
-    @Override
     public TalonFX getBaseController() {
         return _talon;
     }
 
     @Override
+    public void follow(SmartMC masterMC) {
+        if (_onBus) {
+			_ctrlMode = ControlMode.Follower;
+			if (masterMC instanceof  CANTalonFX) {
+				_talon.follow(((CANTalonFX) masterMC).getBaseController());
+			} else if (masterMC instanceof CANTalonSRX) {
+				_talon.follow(((CANTalonSRX) masterMC).getBaseController());
+        	} else {
+	            _talon.set(_ctrlMode, masterMC.getCANID());
+        	}
+        }
+    }
+
+    @Override
+    public double getInputVoltage() {
+        return _onBus ? _talon.getBusVoltage() : Double.NaN;
+    }
+
+    @Override
+    public double getOutputVoltage() {
+        return _onBus ? _talon.getMotorOutputVoltage() : Double.NaN;
+    }
+
+    @Override
+    public double getInputCurrent() {
+        return _onBus ? _talon.getSupplyCurrent() : Double.NaN;
+    }
+
+    @Override
+    public double getOutputCurrent() {
+        return _onBus ? _talon.getStatorCurrent() : Double.NaN;
+    }
+
+    @Override
+    public void setNeutralMode(NeutralMode mode) {
+        if (_onBus) {
+            _talon.setNeutralMode(mode == NeutralMode.kBrake ?
+                    com.ctre.phoenix.motorcontrol.NeutralMode.Brake :
+                    com.ctre.phoenix.motorcontrol.NeutralMode.Coast);
+        }
+    }
+
+    @Override
+    public int getCANID() {
+        return _canID;
+    }
+
+    @Override
+    public void wipeSettings() {
+        if (_onBus) {
+            _talon.configFactoryDefault();
+        }
+    }
+
+    @Override
+    public void limitInputCurrent(int currentLimit) {
+        if (_onBus) {
+            inputCurrentConfig.currentLimit = currentLimit;
+            _talon.configSupplyCurrentLimit(inputCurrentConfig);
+        }
+    }
+
+    public void limitOutputCurrent(int currentLimit) {
+        if (_onBus) {
+            outputCurrentConfig.currentLimit = currentLimit;
+            _talon.configStatorCurrentLimit(outputCurrentConfig);
+        }
+    }
+
+    @Override
+    public double getSensorPosition() {
+        return _onBus ? (_talon.getSelectedSensorPosition() / 2048.0) : Double.NaN;
+    }
+
+    @Override
+    public double getSensorVelocity() {
+        return _onBus ? ((_talon.getSelectedSensorVelocity() / 2048.0) * 600) : Double.NaN;
+    }
+
+    @Override
+    public void setVelocity(double v) {
+        if (_onBus) {
+            _talon.configMotionCruiseVelocity((int) v);
+        }
+    }
+
+    @Override
+    public void rezeroSensor() {
+        if (_onBus) {
+            _talon.setSelectedSensorPosition(0);
+        }
+    }
+
+    @Override
+    public void setEncoderPosition(double position) {
+        if (_onBus) {
+            _talon.setSelectedSensorPosition((int) position);
+        }
+    }
+
+    @Override
+    public void setSensorPhase(boolean phase) { }
+
+    @Override
+    public void set(double power) {
+        if (_onBus) {
+			_ctrlMode = ControlMode.PercentOutput;
+            _talon.set(_ctrlMode, power);
+        }
+    }
+
+    @Override
+    public double get() {
+        return _onBus ? _talon.getMotorOutputPercent() : Double.NaN;
+    }
+
+    @Override
+    public void stop() {
+        if (_onBus) {
+            _talon.set(ControlMode.PercentOutput, 0);
+        }
+    }
+
+    @Override
+    public void setInverted(boolean inverted) {
+        if (_onBus) {
+            _talon.setInverted(inverted);
+        }
+    }
+
+    @Override
+    public boolean getInverted() {
+        return _onBus && _talon.getInverted();
+    }
+
+    @Override
     public void setPIDF(ClosedLoopConfig closedLoopConfig) {
-        _talon.config_kP(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkP());
-        _talon.config_kI(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkI());
-        _talon.config_kD(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkD());
-        _talon.config_kF(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkF());
+        if (_onBus) {
+            _talon.config_kP(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkP());
+            _talon.config_kI(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkI());
+            _talon.config_kD(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkD());
+            _talon.config_kF(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkF());
+        }
     }
 }
--- src/main/java/frc/team832/lib/motorcontrol2/vendor/CANTalonSRX.java
+++ src/main/java/frc/team832/lib/motorcontrol2/vendor/CANTalonSRX.java
@@ -1,25 +1,24 @@
 package frc.team832.lib.motorcontrol2.vendor;
 
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.team832.lib.CANDevice;
 import frc.team832.lib.motorcontrol.NeutralMode;
-import frc.team832.lib.motorcontrol2.PowerManagedMC;
 import frc.team832.lib.motorcontrol2.SmartMC;
 import frc.team832.lib.motors.Motor;
 import frc.team832.lib.util.ClosedLoopConfig;
 
-public class CANTalonSRX extends PowerManagedMC<TalonSRX> {
-    
+public class CANTalonSRX implements SmartMC<TalonSRX> {
     private final TalonSRX _talon;
+
     private final Motor _motor;
+    private final int _canID;
+
+    private boolean _onBus;
 
     private ControlMode _ctrlMode;
-
     private SupplyCurrentLimitConfiguration inputCurrentConfig = new SupplyCurrentLimitConfiguration(true, 40, 0, 0);
-    private StatorCurrentLimitConfiguration outputCurrentConfig = new StatorCurrentLimitConfiguration(true, 40, 0, 0);
 
 
     /***
@@ -31,10 +30,11 @@ public class CANTalonSRX extends PowerManagedMC<TalonSRX> {
 
         _motor = motor;
         _talon = new TalonSRX(canId);
+        _canID = canId;
         _ctrlMode = ControlMode.PercentOutput;
 
-        boolean onBus = _talon.getBusVoltage() > 0.0; // TODO: better way to do this?
-        CANDevice.addDevice(new CANDevice(canId, onBus, "Talon SRX"));
+        _onBus = _talon.getBusVoltage() > 0.0; // TODO: better way to do this?
+        CANDevice.addDevice(new CANDevice(_canID, _onBus, "Talon SRX"));
     }
 
     @Override
@@ -44,7 +44,7 @@ public class CANTalonSRX extends PowerManagedMC<TalonSRX> {
 
     @Override
     public TalonSRX getBaseController() {
-        return null;
+        return _talon;
     }
 
     @Override
@@ -59,105 +59,130 @@ public class CANTalonSRX extends PowerManagedMC<TalonSRX> {
 
     @Override
     public double getInputVoltage() {
-        return _talon.getBusVoltage();
+        return _onBus ? _talon.getBusVoltage() : Double.NaN;
     }
 
     @Override
     public double getOutputVoltage() {
-        return _talon.getMotorOutputVoltage();
+        return _onBus ? _talon.getMotorOutputVoltage() : Double.NaN;
     }
 
     @Override
     public double getInputCurrent() {
-        return _talon.getSupplyCurrent();
+        return _onBus ? _talon.getSupplyCurrent() : Double.NaN;
     }
 
     @Override
     public double getOutputCurrent() {
-        return _talon.getStatorCurrent();
+        return _onBus ? _talon.getStatorCurrent() : Double.NaN;
     }
 
     @Override
     public void setNeutralMode(NeutralMode mode) {
-        _talon.setNeutralMode(mode == NeutralMode.kBrake ?
-         com.ctre.phoenix.motorcontrol.NeutralMode.Brake :
-          com.ctre.phoenix.motorcontrol.NeutralMode.Coast);
+        if (_onBus) {
+            _talon.setNeutralMode(mode == NeutralMode.kBrake ?
+                    com.ctre.phoenix.motorcontrol.NeutralMode.Brake :
+                    com.ctre.phoenix.motorcontrol.NeutralMode.Coast);
+        }
     }
 
     @Override
     public int getCANID() {
-        return _talon.getBaseID();
+        return _canID;
     }
 
     @Override
-    public void wipeSettings () {
-        _talon.configFactoryDefault();
+    public void wipeSettings() {
+        if (_onBus) {
+            _talon.configFactoryDefault();
+        }
     }
 
     @Override
     public void limitInputCurrent(int currentLimit) {
-        inputCurrentConfig.currentLimit = currentLimit;
-        _talon.configSupplyCurrentLimit(inputCurrentConfig);
+        if (_onBus) {
+            inputCurrentConfig.currentLimit = currentLimit;
+            _talon.configSupplyCurrentLimit(inputCurrentConfig);
+        }
     }
 
     @Override
-    public double getSensorPosition () {
-        return _talon.getSelectedSensorPosition();
+    public double getSensorPosition() {
+        return _onBus ? (_talon.getSelectedSensorPosition() / 4096.0) : Double.NaN;
     }
 
     @Override
-    public double getSensorVelocity () {
-        return (_talon.getSelectedSensorVelocity() / 4096.0) * 600;
+    public double getSensorVelocity() {
+        return _onBus ? ((_talon.getSelectedSensorVelocity() / 4096.0) * 600) : Double.NaN;
     }
 
     @Override
-    public void setVelocity (double v) {
-        _talon.configMotionCruiseVelocity((int) v);
+    public void setVelocity(double v) {
+        if (_onBus) {
+            _talon.configMotionCruiseVelocity((int) v);
+        }
     }
 
     @Override
-    public void rezeroSensor () {
-        _talon.setSelectedSensorPosition(0);
+    public void rezeroSensor() {
+        if (_onBus) {
+            _talon.setSelectedSensorPosition(0);
+        }
     }
 
     @Override
-    public void setEncoderPosition (double position) { _talon.setSelectedSensorPosition((int)position); }
-
-    @Override
-    public void set(double power) {
-        _talon.set(_ctrlMode, power);
-    }
-
-    @Override
-    public double get() {
-        return _talon.getMotorOutputPercent();
-    }
-
-    @Override
-    public void stop() {
-        _talon.set(ControlMode.PercentOutput, 0);
-    }
-
-    @Override
-    public void setInverted(boolean inverted) {
-        _talon.setInverted(inverted);
+    public void setEncoderPosition(double position) {
+        if (_onBus) {
+            _talon.setSelectedSensorPosition((int) position);
+        }
     }
 
     @Override
     public void setSensorPhase(boolean phase) {
-        _talon.setSensorPhase(phase);
+        if (_onBus) {
+            _talon.setSensorPhase(phase);
+        }
+    }
+
+    @Override
+    public void set(double power) {
+        if (_onBus) {
+            _ctrlMode = ControlMode.PercentOutput;
+            _talon.set(_ctrlMode, power);
+        }
+    }
+
+    @Override
+    public double get() {
+        return _onBus ? _talon.getMotorOutputPercent() : Double.NaN;
+    }
+
+    @Override
+    public void stop() {
+        if (_onBus) {
+            _talon.set(ControlMode.PercentOutput, 0);
+        }
+    }
+
+    @Override
+    public void setInverted(boolean inverted) {
+        if (_onBus) {
+            _talon.setInverted(inverted);
+        }
     }
 
     @Override
     public boolean getInverted() {
-        return _talon.getInverted();
+        return _onBus && _talon.getInverted();
     }
 
     @Override
     public void setPIDF(ClosedLoopConfig closedLoopConfig) {
-        _talon.config_kP(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkP());
-        _talon.config_kI(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkI());
-        _talon.config_kD(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkD());
-        _talon.config_kF(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkF());
+        if (_onBus) {
+            _talon.config_kP(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkP());
+            _talon.config_kI(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkI());
+            _talon.config_kD(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkD());
+            _talon.config_kF(closedLoopConfig.getSlotIDx(), closedLoopConfig.getkF());
+        }
     }
 }
