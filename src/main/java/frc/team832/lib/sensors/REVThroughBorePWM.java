75c197d005b5816fca8ab97e8d341bad6f219ff2
Banks T <btrout.dhrs@gmail.com>
2020-02-20 03:06:37 -0500

    Add HallEffect, various cleanups.

--- src/main/java/frc/team832/lib/sensors/REVThroughBorePWM.java
+++ src/main/java/frc/team832/lib/sensors/REVThroughBorePWM.java
@@ -1,22 +1,9 @@
 package frc.team832.lib.sensors;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class REVThroughBorePWM extends DutyCycleEncoder {
-
-    private DigitalInput index;
-
     public REVThroughBorePWM(int pwmChannel) {
         super(pwmChannel);
-    }
-
-    public REVThroughBorePWM(int pwmChannel, int indexChannel) {
-        super(pwmChannel);
-        index = new DigitalInput(indexChannel);
-    }
-
-    public boolean getIndex() {
-        return index != null && index.get();
     }
 }
