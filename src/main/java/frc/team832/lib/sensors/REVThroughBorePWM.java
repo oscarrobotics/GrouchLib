3fe11835fb9d57f0ea4454634379807a570d43dc
Banks T <btrout.dhrs@gmail.com>
2020-02-26 04:43:27 -0500

    Sim fix for DutyCycleEncoder

--- src/main/java/frc/team832/lib/sensors/REVThroughBorePWM.java
+++ src/main/java/frc/team832/lib/sensors/REVThroughBorePWM.java
@@ -1,6 +1,6 @@
 package frc.team832.lib.sensors;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.DutyCycleEncoder;
 
 public class REVThroughBorePWM extends DutyCycleEncoder {
     public REVThroughBorePWM(int pwmChannel) {
