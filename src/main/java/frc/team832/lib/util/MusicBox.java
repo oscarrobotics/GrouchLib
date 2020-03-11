3299dbbe8e7560adcffd88122a5d448bfff26380
OSCAR-Programming1 <programming@roswellrobotics.com>
2020-03-11 15:49:35 -0400

    MusicBox update

--- src/main/java/frc/team832/lib/util/MusicBox.java
+++ src/main/java/frc/team832/lib/util/MusicBox.java
@@ -3,34 +3,50 @@ package frc.team832.lib.util;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.music.Orchestra;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team832.lib.Debug;
 import frc.team832.lib.motorcontrol2.vendor.CANTalonFX;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MusicBox {
 
     private final Orchestra _orchestra;
-    private final ArrayList<TalonFX> _instruments = new ArrayList<>();
 
     public MusicBox(CANTalonFX... instruments) {
+        ArrayList<TalonFX> _instruments = new ArrayList<>();
         for (var instrument : instruments) {
             _instruments.add(instrument.getBaseController());
         }
 
         _orchestra = new Orchestra(_instruments);
+        SendableChooser<String> songChooser = new SendableChooser<>();
+
+        try {
+            var lol = Files.list(Filesystem.getDeployDirectory().toPath());
+            lol.forEach((f) ->
+            {
+                var name = f.getFileName().toString().replace(".chrp", "");
+                System.out.println(name);
+                if (f.endsWith(".chrp")) {
+                    songChooser.addOption(name, name);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SmartDashboard.putData("Songs", songChooser);
     }
 
     public void loadSong(String songFileName) {
-        Path songPath = Path.of(Filesystem.getDeployDirectory().getAbsolutePath(),"songs", songFileName);
-        if (!Files.exists(songPath)) {
-            Debug.showDSError(String.format("No song at given path: %s\n", songPath.toString()));
-            return;
-        }
 
-        _orchestra.loadMusic(songPath.toString());
+        _orchestra.loadMusic(songFileName);
     }
 
     public void play() {
